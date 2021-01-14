69d32bf889e24b81c598683a1a35238c1f166b66
sanghyek223 <jsbxiw819224@gmail.com>
2021-01-14 17:59:24 +0900

    지도 위치정보

---
+++ GeolocationAPI/resources/views/layout.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>main</title>
+    <link rel="stylesheet" href="{{mix('css/app.css') }}">
+</head>
+<body>
+    <div id="wrap">
+        <div class="header">
+            <div class="logo">
+                <img src="img/%EB%A1%9C%EA%B3%A0.png" alt="logo">
+            </div>
+            <div class="search">
+                <input type="text" placeholder="">
+            </div>
+                <div class="container">
+                    <div class="menu">
+                        <div class="one"></div>
+                        <div class="two"></div>
+                        <div class="three"></div>
+                    </div>
+                </div>
+        </div><!--header 끝 최종-->
+
+
+        <main>
+        <div class="wrap">
+            <div class="banner">
+                <ul>
+                    <li><img src="img/banner_image1.jpg" alt=""></li>
+                    <!--<li><img src="image/banner/banner_image2.jpg" alt=""></li>
+                    <li><img src="image/banner/banner_image3.jpg" alt=""></li>-->
+                </ul>
+            </div><!--배너박스-->
+            <div class="main_row">
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="main_card">
+                    <li class="card_rayout">
+                        <a href="#"><!--해당 물건 페이지 링크-->
+                        <div class="card_image"><img src="img/goldbar.jpg"
+                     alt="gold_bar"></div><!--이미지-->
+                        <div class="card_info">
+                            <div class="card_title">가나다라 마바사 아자차카 타파하</div><!--제목-->
+                            <div class="card_address">서울시 강동구 동동동</div><!--제목-->
+                            <div class="card_price">999,999,999,999원</div><!--가격-->
+                        </div>
+                        </a>
+                    </li>
+                </ul>
+
+
+            </div>
+        </div>
+    </main>
+
+
+
+
+
+
+        <div class="footer">
+            <ul class="foot">
+                <li><a href="#"><img src="img/home.png" alt="icon"><p>Home</p></a></li>
+                <li><a href="#"><img src="img/chat.png" alt="chat"><p>Chat</p></a></li>
+                <li><a href="#"><img src="img/my-page.png" alt="my page"><p>My page</p></a></li>
+                <li><a href="#"><img src="img/menu.png" alt="menu"><p>Menu</p></a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
