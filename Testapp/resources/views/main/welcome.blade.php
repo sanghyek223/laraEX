be2976e06c085aae577e0b2c147e1800864084db
sanghyek223 <jsbxiw819224@gmail.com>
2021-01-16 12:15:10 +0900

    검수자 페이지 생성 회원가입 구현 / status 확인 가능 / confrim MSG확인가능 (유효성검사 추가해야함)

--- Testapp/resources/views/main/welcome.blade.php
+++ Testapp/resources/views/main/welcome.blade.php
@@ -15,7 +15,55 @@
 @endauth
 @endif
     <li><a href="">상품</a></li>
-
 </ul>
+
+<input type="button" value="click" onclick="geoFindMe()">
+
+<script>
+function geoFindMe() {
+
+function success(position) {
+  const latitude  = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const x_y = latitude +","+longitude;
+  console.log(x_y);
+  test(x_y);
+}
+
+function error() {
+  status.textContent = 'Unable to retrieve your location';
+}
+
+if(!navigator.geolocation) {
+  status.textContent = 'Geolocation is not supported by your browser';
+} else {
+  status.textContent = 'Locating…';
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+}
+
+function test(x_y) {
+    $.ajax({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                },
+                                type: 'post',
+                                url: "{{ route('geo_main') }}",
+                                dataType: 'json',
+                                data: {
+
+                                    data: x_y
+
+                                },
+                                success: function (data) {
+                                        console.log(data);
+                                },
+                                error: function (data) {
+                                    console.log("error" + data);
+                                }
+                            });
+}
+</script>
 @endsection
 
